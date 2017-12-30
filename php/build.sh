8a21ca968fb3c01826528f113d382d57e74ac2d0
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-12-30 14:26:01 -0200

    Updated PHP FPM NGINX scripts to avoid intermitent errors: rm: can't remove file

---
+++ php/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+then
+    echo "Inform the PHP version to Build. eg. '7.2'"
+    exit 1
+fi
+
+if [ ! -z "$3" ]
+then
+    docker login --username "$2" --password "$3" "$4"
+    if [[ $? -ne 0 ]]; then
+        echo "Error"
+        exit 1
+    fi
+fi
+
+if [ ! -f "Dockerfile-$1-cli" ]
+then
+    echo "Does not exists $1"
+    exit 1
+fi
+
+
+#docker rmi byjg/php:$1-cli
+docker build -t byjg/php:$1-cli -f Dockerfile-$1-cli .
+if [[ $? -ne 0 ]]; then
+    echo "Error"
+    exit 1
+fi
+if [ ! -z "$3" ]; then docker push byjg/php:$1-cli; fi
+
+#docker rmi byjg/php:$1-fpm
+docker build -t byjg/php:$1-fpm -f Dockerfile-$1-fpm .
+if [[ $? -ne 0 ]]; then
+    echo "Error"
+    exit 1
+fi
+if [ ! -z "$3" ]; then docker push byjg/php:$1-fpm; fi
+
+#docker rmi byjg/php:$1-fpm-nginx
+docker build -t byjg/php:$1-fpm-nginx -f Dockerfile-$1-fpm-nginx .
+if [[ $? -ne 0 ]]; then
+    echo "Error"
+    exit 1
+fi
+if [ ! -z "$3" ]; then docker push byjg/php:$1-fpm-nginx; fi
