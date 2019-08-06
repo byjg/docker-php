1eaa858bc19f301609094c2325250c2b9d4ad6a4
Joao Gilberto Magalhaes <joao@byjg.com.br>
2019-08-05 21:38:24 -0500

    Simplifying build.sh

--- php/build.sh
+++ php/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
 then
     echo ""
     echo "build.sh <VERSION e.g 7.2> [DOCKERLOGIN] [DOCKERPASSWORD] [REGISTRY]"
@@ -8,7 +8,7 @@ then
     exit 1
 fi
 
-if [ ! -z "$3" ]
+if [[ ! -z "$3" ]]
 then
     docker login --username "$2" --password "$3" "$4"
     if [[ $? -ne 0 ]]; then
@@ -17,48 +17,29 @@ then
     fi
 fi
 
-if [ ! -f "Dockerfile-$1-cli" ]
-then
-    echo "Does not exists $1"
-    exit 1
-fi
+LIST="$1-base $1-cli $1-fpm $1-fpm-nginx $1-fpm-apache"
 
+for item in $LIST
+do
+    #docker rmi byjg/php:$1-base
+    echo "====================================================="
+    echo " Starting: byjg/php:${item}"
+    echo "====================================================="
 
-#docker rmi byjg/php:$1-base
-docker build -t byjg/php:$1-base -f Dockerfile-$1-base .
-if [[ $? -ne 0 ]]; then
-    echo "Error"
-    exit 1
-fi
-if [ ! -z "$3" ]; then docker push byjg/php:$1-base; fi
+    docker build -t byjg/php:${item} -f Dockerfile-${item} .
+    if [[ $? -ne 0 ]]; then
+        echo "Error"
+        exit 1
+    fi
 
-#docker rmi byjg/php:$1-cli
-docker build -t byjg/php:$1-cli -f Dockerfile-$1-cli .
-if [[ $? -ne 0 ]]; then
-    echo "Error"
-    exit 1
-fi
-if [ ! -z "$3" ]; then docker push byjg/php:$1-cli; fi
+    if [[ ! -z "$3" ]]
+    then
+        docker push byjg/php:${item}
 
-#docker rmi byjg/php:$1-fpm
-docker build -t byjg/php:$1-fpm -f Dockerfile-$1-fpm .
-if [[ $? -ne 0 ]]; then
-    echo "Error"
-    exit 1
-fi
-if [ ! -z "$3" ]; then docker push byjg/php:$1-fpm; fi
-
-#docker rmi byjg/php:$1-fpm-nginx
-docker build -t byjg/php:$1-fpm-nginx -f Dockerfile-$1-fpm-nginx .
-if [[ $? -ne 0 ]]; then
-    echo "Error"
-    exit 1
-fi
-if [ ! -z "$3" ]; then docker push byjg/php:$1-fpm-nginx; fi
-
-docker build -t byjg/php:$1-fpm-apache -f Dockerfile-$1-fpm-apache .
-if [[ $? -ne 0 ]]; then
-    echo "Error"
-    exit 1
-fi
-if [ ! -z "$3" ]; then docker push byjg/php:$1-fpm-apache; fi
+        if [[ ! -z "${TRAVIS_BUILD_NUMBER}" ]]
+        then
+            docker tag byjg/php:${item} byjg/php:${item}.${TRAVIS_BUILD_NUMBER}
+            docker push byjg/php:${item}.${TRAVIS_BUILD_NUMBER}
+        fi
+    fi
+done
