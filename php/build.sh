a6cc0a92165462fc9fd9eac5f012729cb834ae33
Joao Gilberto Magalhaes <joao@byjg.com.br>
2020-04-29 23:51:11 -0500

    Try multi cpu arch

--- php/build.sh
+++ php/build.sh
@@ -23,12 +23,17 @@ LIST="$1-base $1-cli $1-fpm $1-fpm-nginx $1-fpm-apache"
 
 for item in $LIST
 do
+    arch=".amd64"
+    if [ ! -z "$TRAVIS_CPU_ARCH" ];
+    then
+      arch=".$TRAVIS_CPU_ARCH"
+    fi
     #docker rmi byjg/php:$1-base
     echo "====================================================="
     echo " Starting: byjg/php:${item}"
     echo "====================================================="
 
-    docker build -t byjg/php:${item} -f Dockerfile-${item} .
+    docker build -t byjg/php:${item}{$arch} -f Dockerfile-${item} .
     if [[ $? -ne 0 ]]; then
         echo "Error"
         exit 1
@@ -36,12 +41,12 @@ do
 
     if [[ ! -z "$3" ]]
     then
-        docker push byjg/php:${item}
+        docker push byjg/php:${item}{$arch}
 
         if [[ ! -z "${TRAVIS_BUILD_NUMBER}" ]]
         then
-            docker tag byjg/php:${item} byjg/php:${item}.${TRAVIS_BUILD_NUMBER}
-            docker push byjg/php:${item}.${TRAVIS_BUILD_NUMBER}
+            docker tag byjg/php:${item}{$arch} byjg/php:${item}.${TRAVIS_BUILD_NUMBER}{$arch}
+            docker push byjg/php:${item}.${TRAVIS_BUILD_NUMBER}{$arch}
         fi
     fi
 done
