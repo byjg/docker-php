e47df4e07e8c32f43dfb2dc38a53977307ad1707
Joao Gilberto Magalhaes <joao@byjg.com.br>
2020-04-30 00:49:23 -0500

    Revert "Try multi cpu arch (3)"
    This reverts commit 3062dde3

--- php/build.sh
+++ php/build.sh
@@ -24,7 +24,7 @@ LIST="$1-base $1-cli $1-fpm $1-fpm-nginx $1-fpm-apache"
 for item in $LIST
 do
     arch=".amd64"
-    if [ -n "$TRAVIS_CPU_ARCH" ];
+    if [ ! -z "$TRAVIS_CPU_ARCH" ];
     then
       arch=".$TRAVIS_CPU_ARCH"
     fi
@@ -33,7 +33,7 @@ do
     echo " Starting: byjg/php:${item}"
     echo "====================================================="
 
-    docker build -t byjg/php:${item}${arch} -f Dockerfile-${item} .
+    docker build -t byjg/php:${item}{$arch} -f Dockerfile-${item} .
     if [[ $? -ne 0 ]]; then
         echo "Error"
         exit 1
@@ -41,12 +41,12 @@ do
 
     if [[ ! -z "$3" ]]
     then
-        docker push byjg/php:${item}${arch}
+        docker push byjg/php:${item}{$arch}
 
         if [[ ! -z "${TRAVIS_BUILD_NUMBER}" ]]
         then
-            docker tag byjg/php:${item}${arch} byjg/php:${item}.${TRAVIS_BUILD_NUMBER}${arch}
-            docker push byjg/php:${item}.${TRAVIS_BUILD_NUMBER}${arch}
+            docker tag byjg/php:${item}{$arch} byjg/php:${item}.${TRAVIS_BUILD_NUMBER}{$arch}
+            docker push byjg/php:${item}.${TRAVIS_BUILD_NUMBER}{$arch}
         fi
     fi
 done
