1d0e537fe33882d3e1e1d3a6e47a8cfffd4cc099
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-10-19 11:23:37 -0400

    Refactor generator logic and synchronize build process
    Replaced `buildah` CLI calls with Dockerfile generation to simplify build processes and improve maintainability. Standardized symlink creation formatting across templates

--- build.py
+++ build.py
@@ -79,7 +79,7 @@ my_parser.add_argument('--push',
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
-gen = Generator(args.PHP_Version, args.Debug, args.push != "")
+
 
 cmd_list = []
 cmd_list.append(args.buildBase) if args.buildBase != "" else None
@@ -90,8 +90,6 @@ cmd_list.append(args.buildFpmNginx) if args.buildFpmNginx != "" else None
 cmd_list.append(args.buildNginx) if args.buildNginx != "" else None
 
 for cmd in cmd_list:
-    gen.manifest_create(cmd)
-    for arch in args.arch.split(","):
-        iid = getattr(gen, "build_" + cmd)(arch)
-        gen.manifest_add(iid, cmd, arch)
-    gen.manifest_push(cmd)
+    gen = Generator(args.PHP_Version, args.Debug, args.push != "")
+    iid = getattr(gen, "build_" + cmd)("aa")
+    print(gen.get_dockerfile_content())
